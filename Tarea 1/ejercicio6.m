5f2bdc604a81c67ae86ba1a271f6c65eeca91aa1
Yostinch <yostinch@gmail.com>
2019-03-12 23:37:19 -0600

    Update ejercicio6.m

--- Tarea 1/ejercicio6.m
+++ Tarea 1/ejercicio6.m
@@ -1,16 +1,26 @@
-%-------------------------
-% Ejercicio 6
-%-------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-amplitude = 10;
-fo        = 200000;
-fs        = 20500;
-duration  = 1;
-t         = 0:1/fs:duration;
-a         = amp*sin(2*pi*fo*t);
+%%%%%%%%%%%%         .Ejercicio 6.         %%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-samplesPerPeriod = fs/fo
-sound(a,fs);
-figure(1);
-plot(t(1:5*samplesPerPeriod), a(1:5*samplesPerPeriod));
+clc;
+clear all;
+
+fs=44100;               %Frecuencia de muestreo para una señal de audio
+t=[0:1/fs:5];           %Vector del tiempo
+fo=input('Digite el valor de la frecuencia: ');  %Frecuencia fo como entrada
+
+cantidad_de_armonicos = 22000/fo;                       %Se calcula el tono + los armonicos
+cantidad_de_armonicos = round(cantidad_de_armonicos);   %Se redondea a un numero entero para poder utilizar el for
+
+notes = [fo];    %Se crea un vector con el valor de fo
+
+for i=2:cantidad_de_armonicos    
+    notes = [notes;fo*i];       %Se calculan las frecuencias armonicas y se guardan en un vector
+end
+
+%Se reproduce el tono
+x = cos(2*pi*notes*t); 
+sig = reshape(x',cantidad_de_armonicos*length(t),1);
+soundsc(sig,fs)
