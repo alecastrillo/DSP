acab9a7a62549d0f84fa718a11abd60323a22864
Yostinch <yostinch@gmail.com>
2019-03-17 20:47:26 -0600

    Update ejercicio6.m

--- Tarea 1/ejercicio6.m
+++ Tarea 1/ejercicio6.m
@@ -2,10 +2,10 @@
 % Ejercicio 6
 %-------------------------
 
-% Se reproduce un tono con todos sus armónicos en el espectro audible
-% Integrantes: Alejandra Castrillo - Justin Chavarria - Cristhian Rojas
+% Se reproduce un tono con todos sus armÃ³nicos en el espectro audible
+% Integrantes: Alejandra Castrillo - Yostin Chavarria - Cristhian Rojas
 
-fs = 44100;                                             % Frecuencia de muestreo para una señal de audio
+fs = 44100;                                             % Frecuencia de muestreo para una seÃ±al de audio
 t  = 0:1/fs:5;                                          % Vector del tiempo
 fo = input('Digite el valor de la frecuencia: ');       % Frecuencia fo como entrada                    
 cantidad_de_armonicos = round(22000/fo);   
