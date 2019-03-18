113025e8dd10fcd9bd2d070b4e0d4bf4aea08a82
Yostinch <yostinch@gmail.com>
2019-03-17 20:46:44 -0600

    Update ejercicio5.m

--- Tarea 1/ejercicio5.m
+++ Tarea 1/ejercicio5.m
@@ -3,24 +3,24 @@
 %-------------------------
 
 % Se grafica la funcion s(n) como muestreo y s(t) como funcion continua superpuestas  
-% Integrantes: Alejandra Castrillo - Justin Chavarria - Cristhian Rojas
+% Integrantes: Alejandra Castrillo - Yostin Chavarria - Cristhian Rojas
 
 function ejercicio5(A,Fo,Fs,C,ti,tf)
     
-    stFunc           = @(A,Fo,Fs,C,t) A*sin((2*pi*Fo*t)+C);     % Función de forma continua
-    snFunc           = @(A,Fo,Fs,C,n) A*sin((2*pi*Fo*n)/Fs+C);  % Función en variable discreta
+    stFunc           = @(A,Fo,Fs,C,t) A*sin((2*pi*Fo*t)+C);     % FunciÃ³n de forma continua
+    snFunc           = @(A,Fo,Fs,C,n) A*sin((2*pi*Fo*n)/Fs+C);  % FunciÃ³n en variable discreta
     samplesPerPeriod = Fs/Fo;                                   % Cantidad de muestras por periodo
     periods          = (tf-ti) * Fo;                            % Cantidad de periodos de ti a tf
     n                = 0:1:floor(periods*samplesPerPeriod);     % Array con las muestras
-    sn               = snFunc(A,Fo,Fs,C,n);                     % Calculo de la señal discreta
+    sn               = snFunc(A,Fo,Fs,C,n);                     % Calculo de la seÃ±al discreta
     t                = linspace(ti,tf,300);                     % Variable de tiempo
-    st               = stFunc(A,Fo,Fs,C,t);                     % Calculo de la señal continua
+    st               = stFunc(A,Fo,Fs,C,t);                     % Calculo de la seÃ±al continua
     
     figure('Name', 'Ejercicio 5');
     
     hold on; 
     stem(n, sn, 'filled', 'g');                                             % Grafica las muestras 
-    plot(linspace(0,floor(periods*samplesPerPeriod),length(st)), st, 'r');  % Grafica la señal continua 
+    plot(linspace(0,floor(periods*samplesPerPeriod),length(st)), st, 'r');  % Grafica la seÃ±al continua 
     hold off;
     axis tight;
     title(" s(n) ");
